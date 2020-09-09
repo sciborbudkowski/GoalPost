38b9a006d9797f75124bd50f441519a7dcc55cc4
Ścibor Budkowski <scibor.budkowski@gmail.com>
2020-09-09 14:02:49 +0200

    startup view created

--- GoalPost/Controller/GoalsVC.swift
+++ GoalPost/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  GoalPost
 //
 //  Created by Ścibor Budkowski on 09/09/2020.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func addGoalBtnPressed(_ sender: Any) {
+    }
 }
-
